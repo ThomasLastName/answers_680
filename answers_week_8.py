faf8808b7d2304048577c2f5058efaf32e7160a0
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-03-18 16:19:02 -0500

    created the answers for weeks 8 and 9

--- answers_week_8.py
+++ answers_week_8.py
@@ -4,3 +4,78 @@
 ### ~~~
 ## ~~~ Dependencies
 ### ~~~
+
+import numpy as np
+
+
+### ~~~
+## ~~~ Possible answer to exercises 1 and 2
+### ~~~
+
+#
+# ~~~ A routine that takes parameters and activation, assembles and returns the shallow NN that they define
+def build_shallow_network(
+        a,      # ~~~ vector of outer weights
+        W,      # ~~~ matrix of inner weights
+        b,      # ~~~ vector of inner biases
+        c,      # ~~~ outer bias
+        sigma   # ~~~ activation function
+    ):
+    #
+    # ~~~ Set expectations
+    if len(W.shape)==1:
+        W = W.reshape(-1,1)
+    H,d = W.shape
+    assert a.shape==(H,)
+    assert b.shape==(H,)
+    #
+    # ~~~ Define a forward passing model subject to those expectations
+    def model(x):
+        #
+        # ~~~ Shape x appropriately
+        x = np.array(x)
+        only_one_input = len(x.shape)==0 or x.shape==(d,)
+        if only_one_input:
+            x = np.array([x])       # ~~~ e.g., convert 1.2 into np.array([1.2]) and [1.2,3.4] into np.array([[1.2,3.4]])
+        if len(x.shape)==1 and d==1:
+            x = x.reshape(-1,d)     # ~~~ e.g., convert [1.2,3.4] into np.array([[1.2],[3.4]]) in the d==1 case
+        n, dim = x.shape
+        assert dim==d               # ~~~ assert that every *row* of x is a datapoint
+        #
+        # ~~~ Compute the forward step
+        output = c + sigma(b+x@W.T)@a
+        #
+        # ~~~ Format the output as desired
+        if only_one_input:
+            output = output.item()
+        else:
+            assert output.shape==(n,)
+        #
+        # ~~~ Return the formatted output
+        return output
+    #
+    # ~~~ Return the model (itself simply a function) that we defined
+    return model
+
+#
+# ~~~ Define a univariate shallow ReLU network with 10 hidden units
+np.random.seed(680)
+H = 10
+W = np.random.normal(size=(H,1))
+a = np.random.normal(size=(H,))
+b = np.random.normal(size=(H,))
+c = 0.4
+ReLU = lambda x: np.maximum(x,0)
+univar_model = build_shallow_network(a,W,b,c,ReLU)
+
+#
+# ~~~ Define a biivariate shallow ReLU network with 10 hidden units
+np.random.seed(680)
+input_dim = 2
+H = 10
+W = np.random.normal(size=(H,input_dim))
+a = np.random.normal(size=(H,))
+b = np.random.normal(size=(H,))
+c = 0.4
+ReLU = lambda x: np.maximum(x,0)
+bivar_model = build_shallow_network(a,W,b,c,ReLU)
--- answers_week_9.py
+++ answers_week_9.py
@@ -4,3 +4,48 @@
 ### ~~~
 ## ~~~ Dependencies
 ### ~~~
+
+import numpy as np
+
+
+
+def par_grad_lacking_bias( a, W, x, sigma, sigma_prime, flatten_and_concatenate=True ):
+    H = len(a)
+    d = len(x)  # ~~~ note that x is assumed to have a length; this fails if, e.g., x=2.2; this function assumes x=[2.2] instead
+    assert W.shape==(H,d)
+    assert a.shape==(H,)
+    assert x.shape==(d,)
+    Wx = W@x    # ~~~ don't bother computing this twice
+    #
+    # ~~~ The partial derivatives of f(a,W) = a^T\sigma(Wx) with respect to a is \sigma(Wx)
+    grad_a = sigma(Wx)
+    #
+    # ~~~ The partial derivative of f(a,W) = a^T\sigma\sigma(Wx) w.r.t. W_{i,j} is a_i\sigma'(\sum_jw_{i,f}x_f)x_j which equals the outer product:
+    grad_W = np.outer( a*sigma_prime(Wx), x )
+    #
+    # ~~~ Return the things that you'd flatten and concatenate to get the gradient vector of f(a,W) = a^T\sigma\sigma(Wx)
+    return np.concatenate(( grad_a, grad_W.flatten() )) if flatten_and_concatenate else (grad_a,grad_W)
+
+
+
+def par_grad_with_bias( a, W, b, c, x, sigma, sigma_prime, flatten_and_concatenate=True ):
+    x_tilde = np.concatenate((x,[1.]))
+    W_tilde = np.column_stack((W,b))
+    grad_a, grad_W_tilde = par_grad_lacking_bias( a, W_tilde, x_tilde, sigma, sigma_prime, flatten_and_concatenate=False )
+    grad_W, grad_b = grad_W_tilde[:,:-1], grad_W_tilde[:,-1]
+    grad_c = 1.
+    return np.concatenate(( grad_a, grad_W.flatten(), grad_b, [grad_c] )) if flatten_and_concatenate else ( grad_a, grad_W, grad_b, grad_c )
+
+
+def grad_of_item_loss( a, W, b, c, x, y, sigma, sigma_prime, ell_prime, flatten_and_concatenate=True ):
+    x = np.array(x)
+    assert len(x.squeeze().shape)<2 # ~~~ x is expected to be a single datapoint, *not* a list of several datapoints
+    assert isinstance(y,float)      # ~~~ y is expected to be a single (float type) label, *not* a list of several labels
+    x = x.reshape(-1)               # ~~~ turn 1.2 into [1.2] in order to enable the syntax W@x
+    pred = c + np.inner(a,sigma(W@x+b))
+    if flatten_and_concatenate:
+        return ell_prime(y,pred)*par_grad_with_bias( a, W, b, c, x, sigma, sigma_prime )
+    else:
+        grad_a, grad_W, grad_b, grad_c = par_grad_with_bias( a, W, b, c, x, sigma, sigma_prime, flatten_and_concatenate=False )
+        return ell_prime(y,pred)*grad_a, ell_prime(y,pred)*grad_W, ell_prime(y,pred)*grad_b, ell_prime(y,pred)*grad_c 
+
