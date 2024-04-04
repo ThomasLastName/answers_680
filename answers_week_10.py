54b406b5c47f9db33933c070a85997591c83210b
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-04-04 07:41:33 -0500

    moved definition of my_cross_entropy into the answers file

--- answers_week_10.py
+++ answers_week_10.py
@@ -4,3 +4,12 @@
 ### ~~~
 ## ~~~ Dependencies
 ### ~~~
+
+import torch
+from quality_of_life.my_torchvision_utils import hot_1_encode_an_integer
+
+def my_cross_entropy(predicted,targets):
+    encode = hot_1_encode_an_integer(n_class=5)
+    t = encode(targets)
+    p = predicted.softmax(dim=1)
+    return ( -t*p.log() ).sum(axis=1).mean()
