135a6f97115e8d228a503523ca55a680545102ae
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-04-17 14:15:56 -0500

    Created answers for week_12

--- answers_week_12.py
+++ answers_week_12.py
@@ -4,3 +4,23 @@
 ### ~~~
 ## ~~~ Dependencies
 ### ~~~
+
+import torch
+from quality_of_life.my_torch_utils import hot_1_encode_an_integer
+
+
+### ~~~
+## ~~~ Possible answers
+### ~~~
+
+def measure_accuracy(model,X_test,y_test):
+    with torch.no_grad():
+        acc = ( model(X_test).argmax(dim=1) == y_test ).float().mean().item()   # ~~~ n_correct==( model(X_test).argmax(dim=1) == y_test ).sum()
+    return acc
+
+
+def my_cross_entropy(predicted,targets):
+    encode = hot_1_encode_an_integer(n_class=5)
+    t = encode(targets)
+    p = predicted.softmax(dim=1)
+    return ( -t*p.log() ).sum(axis=1).mean()
