84134dc020c8e33c1060acf28a7ff9d82e160b28
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2025-01-13 13:00:33 -0600

    What about this?

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "answers_680",
@@ -6,6 +6,8 @@ setup(
     description = "Answers to the exercises in labs_680",
     author = "Thomas Winckelman",
     author_email = "winckelman@tamu.edu",
-    py_modules = [ f"answers_week_{j+1}" for j in range(15) ],  # List of top-level Python files (without .py)
-    packages = find_packages()
+    packages = [ "answers_680" ],
+    py_modules = [ f"answers_week_{j+1}" for j in range(15) ],
+    packages = find_packages(),
+    include_package_data = True
 )
