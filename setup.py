2ca822139f0a032dd55c2f54a2f6e414274f3826
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2025-01-13 12:38:14 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "answers_680",
+    version="1.0.0",
+    description = "Answers to the exercises in labs_680",
+    author = "Thomas Winckelman",
+    author_email = "winckelman@tamu.edu",
+    py_modules = [ f"answers_week_{j+1}" for j in range(15) ],  # List of top-level Python files (without .py)
+)
